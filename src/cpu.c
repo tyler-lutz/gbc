67f5bee567e360d246b81d19e5c46e7f654f3049
Tyler Lutz <tyleralexanderlutz@gmail.com>
2025-09-02 09:15:10 -0400

    feat: add clang-format

--- src/cpu.c
+++ src/cpu.c
@@ -5,13 +5,13 @@
 void cpu_init(struct CPU *cpu) { memset(cpu, 0, sizeof(*cpu)); }
 
 void cpu_reset(struct CPU *cpu) {
-  memset(cpu, 0, sizeof(struct CPU));
+    memset(cpu, 0, sizeof(struct CPU));
 
-  cpu->AF = 0x01B0;
-  cpu->BC = 0x0013;
-  cpu->DE = 0x00D8;
-  cpu->HL = 0x014D;
+    cpu->AF = 0x01B0;
+    cpu->BC = 0x0013;
+    cpu->DE = 0x00D8;
+    cpu->HL = 0x014D;
 
-  cpu->SP = 0xFFFE;
-  cpu->PC = 0x0100;
+    cpu->SP = 0xFFFE;
+    cpu->PC = 0x0100;
 }
--- src/cpu.h
+++ src/cpu.h
@@ -4,40 +4,40 @@
 #include <stdint.h>
 
 struct CPU {
-  union {
-    struct {
-      uint8_t F;
-      uint8_t A;
+    union {
+        struct {
+            uint8_t F;
+            uint8_t A;
+        };
+        uint16_t AF;
     };
-    uint16_t AF;
-  };
 
-  union {
-    struct {
-      uint8_t C;
-      uint8_t B;
+    union {
+        struct {
+            uint8_t C;
+            uint8_t B;
+        };
+        uint16_t BC;
     };
-    uint16_t BC;
-  };
 
-  union {
-    struct {
-      uint8_t E;
-      uint8_t D;
+    union {
+        struct {
+            uint8_t E;
+            uint8_t D;
+        };
+        uint16_t DE;
     };
-    uint16_t DE;
-  };
 
-  union {
-    struct {
-      uint8_t L;
-      uint8_t H;
+    union {
+        struct {
+            uint8_t L;
+            uint8_t H;
+        };
+        uint16_t HL;
     };
-    uint16_t HL;
-  };
 
-  uint16_t SP; // Stack Pointer
-  uint16_t PC; // Program Counter
+    uint16_t SP; // Stack Pointer
+    uint16_t PC; // Program Counter
 };
 
 void cpu_reset(struct CPU *cpu);
